d90bc0b793e306fdf412ae0bd7b3c4ef145fb071
Xavier Carty <xaviercarty@gmail.com>
2020-04-17 12:34:50 -0400

    final project

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,8 +3,8 @@ class Contract < ApplicationRecord
     belongs_to :team 
     belongs_to :player
 
-    validates :length, numericality: {  
-    less_than_or_equal_to: 3 }
+    validates :length, numericality: { greater_than_or_equal_to:1 ,  
+    less_than_or_equal_to: 5 }
     validates :player_id, uniqueness: true 
    
 end
